62ecfdfba29cbe5ee5d7bba76b31255ba8828164
gordonwoodbine <gordon_woodbine@yahoo.co.uk>
2021-03-25 22:47:34 +0000

    add local storage, image hover state

--- cat.js
+++ cat.js
@@ -1,10 +1,12 @@
 
-const catImg = document.querySelector('.cat-image');
+// Cache what we need
 const catLoader = document.querySelector('.cat-loading');
 const catGallery = document.querySelector('.cat-gallery');
 let catURL = 'https://api.thecatapi.com/v1/images/search';
 
-async function init() {
+function init() {
+  const catImg = document.querySelector('.cat-image');
+
   catImg.addEventListener('load', () => {
     catLoader.classList.remove('show');
     catImg.classList.add('show');
@@ -12,40 +14,98 @@ async function init() {
 
   const newCatBtn = document.querySelector('.new-btn');
   newCatBtn.addEventListener('click', () => {
-    showNewCat();
+    showNewCat(catImg);
   });
 
   const saveCatBtn = document.querySelector('.save-btn');
-  saveCatBtn.addEventListener('click', saveCat);
+  saveCatBtn.addEventListener('click', () => {
+    saveCat(catImg.src);
+  });
 
-  showNewCat();
+  catGallery.addEventListener('click', checkAndDelete);
+
+  // if any saved cats are in local storage, show them
+  getLocalCats();
+
+  // get an initial cat to show
+  showNewCat(catImg);
 }
 
-async function showNewCat() {
+// Request new cat from API and display it
+async function showNewCat(catImg) {
   catImg.classList.remove('show');
   catLoader.classList.add('show');
-  
-  // const res = await fetch(dogURL);
+
   const res = await fetch(catURL);
   const data = await res.json();
-  // console.log(data[0].url);
 
   catImg.src = data[0].url;
 }
 
-function saveCat() {
-  console.log(catGallery.children);
+// Add cats to favourites list
+function saveCat(url) {
+  // console.log(catGallery.children);
   document.querySelector('.fave-title').style.visibility = 'visible';
+
+  showInFavourites(url);
+  saveLocalCats(url);
+}
+
+// Display cat picture in favourites section
+function showInFavourites(url) {
+  // Create a figure element ...
   const newFigure = document.createElement('figure');
   newFigure.classList.add('gallery-item');
 
+  // ... then create an img element
   const newImg = document.createElement('img');
   newImg.classList.add('gallery-img');
-  newImg.src = catImg.src;
+  newImg.src = url;
   newImg.alt = 'lovely cat';
 
+  // .. then create an overlay ...
+  const overlay = document.createElement('div');
+  overlay.classList.add('overlay');
+  overlay.innerHTML = '<i class="icon fas fa-times"></i>';
+
+  // ... then mash them together and append to the DOM
   newFigure.appendChild(newImg);
+  newFigure.appendChild(overlay);
   catGallery.appendChild(newFigure);
+}
+
+// Save favourites list to local storage
+function saveLocalCats(cat) {
+  let catArray;
+  if(localStorage.getItem('catArray') === null) {
+    catArray = []
+  } else {
+    catArray = JSON.parse(localStorage.getItem('catArray'));
+  }
+  catArray.push(cat);
+  localStorage.setItem('catArray', JSON.stringify(catArray));
+}
+
+// Retrieve favourites list from local storage
+function getLocalCats() {
+  let catArray;
+  if(localStorage.getItem('catArray') === null) {
+    catArray = [];
+  } else {
+    catArray = JSON.parse(localStorage.getItem('catArray'));
+  }
+  document.querySelector('.fave-title').style.visibility = 'visible';
+
+  catArray.forEach(cat => {
+    showInFavourites(cat);
+  });
+}
+
+function checkAndDelete(e) {
+  if(e.target.classList[0] === 'gallery-img') {
+    // catGallery.removeChild(e.target.parentElement);
+    const targetURL = e.target.src;
+  }
 }
 
 init();
